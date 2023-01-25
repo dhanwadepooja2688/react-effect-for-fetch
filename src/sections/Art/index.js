7958e75428e8f58850819f333b7d6e00f3f0e40b
pooja <dhanwade.pooja2688@gmail.com>
2023-01-25 17:14:28 +0000

    Basic done

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,10 +1,55 @@
 import { useEffect, useState } from "react"
+import Artlist from "./components/ArtList"
 
 function ArtsSection() {
+const [artworks,setArtworks]=useState([])
+
+useEffect(()=>{
+  fetch("https://api.artic.edu/api/v1/artworks")
+  .then((res) => res.json())
+  .then((data)=>{
+
+    setArtworks(data.data)
+    console.log(data)
+  })
+},[])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+      <ul class="art-list">
+     
+        {artworks.map((art,index) => {
+           return(
+              <li>
+                <div class="frame">
+                  <img
+                    src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+
+                  />
+                </div>
+                <h3>{art.title}</h3>
+                <p>Artist: {art.artist_title}</p>
+                <h4>Artistic Subjects:</h4>
+                <ul>
+                    {art.subject_titles.map((term,index) =>{
+                      return(
+                        <li>{term}</li>
+
+                        )
+                  
+                      })
+                      }
+                  
+                </ul>
+              </li>
+            )
+          })}
+
+        
+      </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,10 +1,39 @@
 import { useEffect, useState } from "react"
 
 function UsersSection() {
+  const [users,setUsers]=useState([])
+  
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=10")
+    .then((res) => res.json())
+    .then((data)=>{
+      setUsers(data.results)
+      
+    })
+  },[])
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+           
+      <ul class="users-list">
+        {users.map((user) =>{
+           return (
+              <li class={`${user.gender === "female" ? "bg-pink" : "bg-blue"}`}>
+              <img
+              src={user.picture.medium}
+              alt="Ritthy Ryan"
+              />
+              <h3>{user.name.title} {user.name.first}{user.name.last}
+              </h3>
+              <p>Email: {user.email}</p>
+              </li>
+           )
+           })}
+      
+      
+       </ul>
+      </div>
     </section>
   )
 }
